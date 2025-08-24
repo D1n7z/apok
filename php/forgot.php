7048b6e8bb3bb199a1cda285d85eab3fb15007b0
Eduardo Diniz <ed.dinizsil@gmail.com>
2025-08-24 02:14:19 -0300

    Update forgot.php

--- php/forgot.php
+++ php/forgot.php
@@ -16,7 +16,7 @@ if ($retorno) {
 
     $mail = new PHPMailer(true);
     try {
-        $mail->SMTPDebug = 0;
+        $mail->SMTPDebug = \PHPMailer\PHPMailer\SMTP::DEBUG_SERVER;
         $mail->isSMTP();
         $mail->Host       = 'smtp-relay.brevo.com'; 
         $mail->SMTPAuth   = true;
