cb9258d5102a3e0a3348303faad103b996001eb4
Eduardo Diniz <ed.dinizsil@gmail.com>
2025-08-24 11:16:23 -0300

    css

--- php/dashboard.php
+++ php/dashboard.php
@@ -13,6 +13,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale-1.0">
     <title>Dashboard</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <h1> Bem vindo(a), <?php echo htmlspecialchars($userName); ?>!</h1>
