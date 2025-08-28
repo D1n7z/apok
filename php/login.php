504580268180b2a179b62c1a0c07929d1c9bf17f
Eduardo Diniz <ed.dinizsil@gmail.com>
2025-08-28 16:35:52 -0300

    Update login.php

--- php/login.php
+++ php/login.php
@@ -6,6 +6,22 @@
     $email = $_POST['txtEmail'];
     $passwd = $_POST['txtSenha'];
 
+
+    $admin_email = "admin@email.com";
+    $admin_password = "senha_forte_admin"; 
+
+    if ($email === $admin_email && $passwd === $admin_password) {
+        $_SESSION['user_id'] = 'admin_id'; 
+        $_SESSION['user_name'] = 'Administrador';
+        $_SESSION['loggedin'] = true;
+        $_SESSION['is_admin'] = true;
+
+        header('Location: admin_dashboard.php'); 
+        exit();
+    }
+
+
+
     $player = getPlayerByEmail($email);
 
     if ($player && password_verify($passwd, $player['senha_hash'])) {
@@ -13,6 +29,7 @@
         $_SESSION['user_id'] = $player['id_player'];
         $_SESSION['user_name'] = $player['nome'];
         $_SESSION['loggedin'] = true; 
+        $_SESSION['is_admin'] = false; 
         
         header('Location: dashboard.php');
         exit(); 
